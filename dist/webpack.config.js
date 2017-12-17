05fba5aa969e3614cf083ad5174c8a2415039d00
Mou Xuekai <mouxuekai@gmail.com>
2017-12-17 18:16:46 +0800

    improve webpack

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -2,18 +2,31 @@
 
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   plugins: [new webpack.optimize.ModuleConcatenationPlugin(), new webpack.LoaderOptionsPlugin({
     minimize: true
+  }), new HtmlWebpackPlugin({
+    filename: 'test.html',
+    template: path.resolve(__dirname, './src/default_test.html')
+  }), new CleanWebpackPlugin([path.resolve(__dirname, './public/javascripts')]), new webpack.HashedModuleIdsPlugin(), new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor'
+  }), new webpack.optimize.CommonsChunkPlugin({
+    name: 'runtime'
   })],
+
   entry: {
-    main: path.resolve(__dirname, './src/javascripts/app.js')
+    app: path.resolve(__dirname, './src/javascripts/app.js'),
+    vendor: ['react', 'react-dom']
   },
+
   output: {
-    path: path.resolve(__dirname, './public/javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './public'),
+    filename: 'javascripts/[name].[chunkhash].bundle.js'
   },
+
   module: {
     rules: [{
       test: /\.js$/,
