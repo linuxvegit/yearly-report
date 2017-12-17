e138d5dc3ca1d01cdd9e154aa5a76be693f9f6b8
Mou Xuekai <mouxuekai@gmail.com>
2017-12-17 20:33:37 +0800

    test some basic react method

--- src/javascripts/PicIcon.js
+++ src/javascripts/PicIcon.js
@@ -4,9 +4,22 @@ import icon from '../images/pic.svg';
 import '../stylesheets/pic-icon.less';
 
 export default class PicIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
   render() {
     return (
-        <img src={icon} className="pic-icon"/>
+        <img src={icon} className="pic-icon" alt="Images"/>
     );
   }
 };
